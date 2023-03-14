62d9adeeb09003cdfdd90266519064977c8398d0
David Jandron <jandron1211@gmail.com>
2023-03-14 09:00:32 -0700

    ADD models message and migrate

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System;
 
 namespace ToDoList.Models
@@ -6,7 +7,9 @@ namespace ToDoList.Models
   public class Item
   {
     public int ItemId { get; set; }
+    [Required(ErrorMessage = "The item's description can't be empty!")]
     public string Description { get; set; }
+   [Range(1, int.MaxValue, ErrorMessage = "You must add your item to a category. Have you created a category yet?")]
     public int CategoryId { get; set; }
     public bool IsComplete { get; set; } = false;
     public DateTime? DueDate { get; set; }
